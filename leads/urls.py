efb172f5126563ba99ceb8f518fe0d65626dea4b
nolan <nolan.braman@gmail.com>
2020-10-11 20:09:02 -0400

    added leads details pages

--- leads/urls.py
+++ leads/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import LeadListView
+from .views import LeadListView, LeadDetailView
 
 urlpatterns = [
-    path('', LeadListView.as_view(), name="lead_list")
+    path('', LeadListView.as_view(), name="lead_list"),
+    path('<int:pk>', LeadDetailView.as_view(), name="lead_detail")
 ]
--- leads/views.py
+++ leads/views.py
@@ -6,3 +6,7 @@ class LeadListView(ListView):
     model = Lead
     context_object_name = "lead_list"
     template_name = 'leads/lead_list.html'
+
+class LeadDetailView(DetailView):
+    model = Lead
+    template_name = 'leads/lead_detail.html'
