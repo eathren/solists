abf5e60bf1d2622d254c867775d589d293d9d86e
eathren <nolan.braman@gmail.com>
2020-10-13 15:53:59 -0400

    added scss, css, navbar

--- solists/urls.py
+++ solists/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
     # Django admin
     path('admin-other/', admin.site.urls),
 
