5bb955aa5de9c17a75216974f717449b4ecc20bf
eathren <nolan.braman@gmail.com>
2020-10-13 14:14:10 -0400

    re-added static

--- solists/settings.py
+++ solists/settings.py
@@ -147,13 +147,13 @@ USE_TZ = True
 
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-# # Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
--- solists/urls.py
+++ solists/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('leads/', include('leads.urls')),
     path('orders/', include('orders.urls'))
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 
 # if settings.DEBUG:
 #     import debug_toolbar
