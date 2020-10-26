de7786c6c268bcd32bde39b489f577802def4e4d
eathren <nolan.braman@gmail.com>
2020-10-26 18:06:19 -0400

    added terms and privacy page

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, TermsOfServicePageView, PrivacyPolicyPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('terms-of-service/', TermsOfServicePageView.as_view(), name='terms_of_service'),
+    path('privacy-policy/', PrivacyPolicyPageView.as_view(), name='privacy_policy')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class TermsOfServicePageView(TemplateView):
+    template_name="terms_of_service.html"
+
+class PrivacyPolicyPageView(TemplateView):
+    template_name="privacy_policy.html"
