cd80a7e1af4e3fb2b641c26bad023da3db5c576c
eathren <nolan.braman@gmail.com>
2020-11-07 15:32:13 -0500

    added hours cusotm tag replace

--- leads/templatetags/custom_tags.py
+++ leads/templatetags/custom_tags.py
@@ -5,4 +5,4 @@ register = template.Library()
 @register.filter(is_safe=True)
 @stringfilter
 def upto(value, delimiter=None):
-    return value.split(delimiter)[0].replace("days", "d").replace("day","d").replace("weeks","w").replace("week","w").replace("months","m").replace("minutes","m").replace("month","m").replace("minute","m").replace("\xa0","")
+    return value.split(delimiter)[0].replace("days", "d").replace("day","d").replace("weeks","w").replace("week","w").replace("months","m").replace("minutes","m").replace("month","m").replace("minute","m").replace("hours","h").replace("hour","h").replace("\xa0","")
