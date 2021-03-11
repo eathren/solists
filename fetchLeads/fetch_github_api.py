f69c24190ad54e81ee8cf22afe4283e2a99b49f1
nolan <nolan.braman@gmail.com>
2021-03-10 21:20:07 -0500

    working auto skills

--- fetchLeads/fetch_github_api.py
+++ fetchLeads/fetch_github_api.py
@@ -21,49 +21,51 @@ def save_new_jobs():
     json_data = get_jobs_json()
     now = datetime.utcnow()
     today_data = True
-    
-    LEAD_SKILLS = Leads.LEAD_SKILLS
-        
+    print('here11')
+    LEAD_SKILLS = Lead.LEAD_SKILLS
+    print('here12')
     if json_data is not None:
-        while today_data == True:
-            for job in json_data:
-                try: 
-                    date_difference = now -  datetime.strptime(job["created_at"],  "%a %b %d %H:%M:%S %Z %Y")
-                    print("Days", date_difference.days)
-                    if date_difference.days == 0:
-                        
-                        new_job = Lead()
-                        new_job.author = "Github Jobs"
-                        new_job.developer = True
-                        new_job.description = job["description"]
-                        new_job.skills = ()
-                        shared = set(x for x, _ in LEAD_SKILLS) & set(list(text.split()))
-                        for item in shared:
-                            new_job.skills += (item , )
-                        
-                        new_job.how_to_apply = job["how_to_apply"]
-                        new_job.author = "Github Jobs"
-                        
-                        if job["type"] == "Full Time":
-                            new_job.job_type = 'Full-time'
-
-                        elif job["type"] == "Contract":
-                            new_job.job_type = 'Contract'
-
-                        new_job.title = job["title"]
-                        new_job.company = job["company"]
-                        application_url =  re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", job["how_to_apply"] )
-                        new_job.application_link = application_url[0]
-                        new_job.user_author =  get_user_model().objects.get(pk='f3ee3abe-a186-44da-9ed2-b18ec3b570b7')
-                        new_job.remote = 'Remote'
-                        new_job.role = 'Developer'
-                        new_job.save()
-                        print('SUCCESS', new_job.title)
-                    else:
-                        print("No new data")
-                        today_data = False
-                        return today_data
+    # while today_data == True:
+        for job in json_data:
+            try: 
+                date_difference = now -  datetime.strptime(job["created_at"],  "%a %b %d %H:%M:%S %Z %Y")
+                print("Days", date_difference.days)
+                if date_difference.days ==0:
+                
+                    new_job = Lead()
+                    new_job.author = "Github Jobs"
+                    new_job.developer = True
+                    new_job.description = job["description"]
+                    new_job.skills = ()
+                    print("test22")
+                    shared = set(x for x, _ in LEAD_SKILLS) & set(list(new_job.description.upper().split()))
+                    print('test28')
+                    for item in shared:
+                        new_job.skills += (item , )
+                    print('test33')
+                    new_job.how_to_apply = job["how_to_apply"]
+                    new_job.author = "Github Jobs"
                     
-                except:
-                    print("FAILED")
-                    pass
+                    if job["type"] == "Full Time":
+                        new_job.job_type = 'Full-time'
+
+                    elif job["type"] == "Contract":
+                        new_job.job_type = 'Contract'
+
+                    new_job.title = job["title"]
+                    new_job.company = job["company"]
+                    application_url =  re.search(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", job["how_to_apply"] )
+                    new_job.application_link = application_url[0]
+                    new_job.user_author =  get_user_model().objects.get(pk='f3ee3abe-a186-44da-9ed2-b18ec3b570b7')
+                    new_job.remote = 'Remote'
+                    new_job.role = 'Developer'
+                    new_job.save()
+                    print('SUCCESS', new_job.title)
+                else:
+                    print("No new data")
+                    today_data = False
+                    return today_data
+                    
+            except:
+                print("FAILED")
+                pass
